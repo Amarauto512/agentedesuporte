ba4891fb54e22db0063b4dea03371efe42e054d5
Amarauto512 <adenamaral12@gmail.com>
2025-05-20 19:32:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def suporte():
     return jsonify({"resposta": resposta.strip()})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
 
